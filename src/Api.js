8e978fa03e2261d8dee8960f74c23253844ee1b0
Vineetmorankar <vineetmorankar@gmail.com>
2024-01-14 13:51:13 +0530

    added new project

---
+++ src/Api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "4f386da116msh464b686c3ec2d90p1a5706jsn085aa59e63bd",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "2f6c671d158b934b6ec7f6871cd48fd6";
