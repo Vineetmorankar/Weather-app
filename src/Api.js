f1469accf979d0f6d6b456877b2a7d1da5fa1627
Vineetmorankar <vineetmorankar@gmail.com>
2024-01-15 01:07:44 +0530

    removed api key

--- src/Api.js
+++ src/Api.js
@@ -1,8 +1,8 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "4f386da116msh464b686c3ec2d90p1a5706jsn085aa59e63bd",
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    "X-RapidAPI-Key": "",
+    "X-RapidAPI-Host": "",
   },
 };
 
